2d65d82643f6cee23c87a0009bb94c10f0ab0f80
Colin Reeder <vpzomtrrfrt@gmail.com>
2015-05-16 12:40:02 -0600

    Actually do stuff

--- villagerLoader.py
+++ villagerLoader.py
@@ -1,9 +1,16 @@
 import os
+import urllib2
+import urllib
+import sys
+import shutil
 dn = ".villagerLoader"
 config = None
 def read(fn):
 	try:
-		h = open(fn)
+		if 'str' in str(type(fn)):
+			h = open(fn)
+		else:
+			h = fn
 		l = h.readlines()
 		h.close()
 		r = {}
@@ -31,8 +38,45 @@ if config == None:
 	tw = ""
 	for o in co:
 		tw+=o[0]+"]["+o[2]+"\n"
-	os.mkdir('.villagerLoader')
+	if not os.path.isdir(dn):
+		os.mkdir('.villagerLoader')
 	h = open(dn+"/config.txt",'w+')
 	h.write(tw)
 	h.close()
 	config = read(dn+"/config.txt")
+url = config["src"][0]
+urllib.urlretrieve(url, dn+"/newThing.txt")
+nd = read(dn+"/newThing.txt")
+ofn = dn+"/lastUpdated.txt"
+od = None
+if os.path.isfile(ofn):
+	od = read(ofn)
+	for d in od:
+		tr = False
+		if d in nd:
+			if nd[d] != od[d]:
+				tr = True
+		else:
+			tr = True
+		if tr:
+			print "Removing "+od[d][0]
+			os.remove(config["dir"][0]+"/"+od[d][0])
+shutil.move(dn+"/newThing.txt", ofn)
+dat1 = False
+for d in nd:
+	td = False
+	if od != None:
+		if d not in od:
+			td = True
+		else:
+			if od[d] != nd[d]:
+				td = True
+	else:
+		td = True
+	if td:
+		print "Downloading "+nd[d][0]
+		dat1 = True
+		sys.stdout.flush()
+		urllib.urlretrieve(nd[d][1], config["dir"][0]+"/"+nd[d][0])
+if not dat1:
+	print "Up to date!"
